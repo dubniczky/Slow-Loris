e7f2b622d3218c29e7c756b78176618a0b4b5a81
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-21 12:26:24 +0200

    Fixed file read to remove new line characters

--- agents.py
+++ agents.py
@@ -2,9 +2,13 @@ import random
 
 # Read agents
 user_agents = None
-with open('data/user-agents.txt', 'o') as f:
-    user_agents = f.readlines()
+with open('data/user-agents.txt', 'r') as f:
+    user_agents = f.read().splitlines()
 
 # Select random agent 
 def random_agent():
     return random.choice(user_agents)
+
+if __name__ == '__main__':
+    print(user_agents[0])
+    print(user_agents[-1])
