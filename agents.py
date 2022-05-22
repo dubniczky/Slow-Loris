127effdc8db7b0b377f3af28eac414b43af36658
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-22 20:33:01 +0200

    Moved random agent to agents file

--- agents.py
+++ agents.py
@@ -5,6 +5,10 @@ user_agents = None
 with open('data/user-agents.txt', 'r') as f:
     user_agents = f.read().splitlines()
 
+# Select random agent
+def random_agent():
+    return random.choice(user_agents)
+
 if __name__ == '__main__':
     print(user_agents[0])
     print(user_agents[-1])
